f9a21bba56908215063334af0f22319e8d7b5306
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2023-02-20 01:20:22 +0600

    er3

--- IOT/settings.py
+++ IOT/settings.py
@@ -8,8 +8,10 @@ SECRET_KEY = 'django-insecure-y0oe7p%5y03665t$$v5##nv08-79!t3ey1vlovid5&p7g!s6yg
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['https://iot-biometric-server-production.up.railway.app']
+CORS_ORIGIN_WHITELIST = [
+    'https://iot-biometric-server-production.up.railway.app',
+]
 # Application definition
 
 INSTALLED_APPS = [
