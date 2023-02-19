b2a8805fa29012d19e99db9b1619b00a6022fddf
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2023-02-20 00:58:04 +0600

    deploy ready

--- IOT/settings.py
+++ IOT/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,5 +104,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
